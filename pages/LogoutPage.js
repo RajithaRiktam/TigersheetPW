bd205cd169ef1e5ae792a0adf3c5d1c879a1dd10
crajitha <rajitha@universaltennis.com>
2024-05-09 17:23:01 +0530

    test branch commit

--- pages/LogoutPage.js
+++ pages/LogoutPage.js
@@ -10,12 +10,12 @@ exports.LogoutPage = class LogoutPage {
         this.logo = page.locator('.text-logo');
     }
     async logOut() {
-       
+
         await this.page.locator('div').filter({ hasText: testData.details.tigersheetLabel }).locator('svg').first().click();
         await this.logout.click();
         await this.page.waitForTimeout(2000);
         const Label = await this.page.locator(this.label).textContent();
-        console.log("This is testdata details RTLABEl - ",testData.details.rtLabel);
+        console.log("This is testdata details RTLABEl - ", testData.details.rtLabel);
         expect(Label).toBe(testData.details.rtLabel);
 
 
